7b185a0e9b3e7875bd1e8fdc4836fa39782dee0f
Andrius Dereviancenko a.dereviancenko@gmail.com <a.dereviancenko@gmail.com>
2026-04-20 19:23:10 +0200

    Fix restrict_format_access for html format in Rails 8

--- config/initializers/active_admin_patches.rb
+++ config/initializers/active_admin_patches.rb
@@ -1,7 +1,7 @@
 ActiveSupport.on_load(:active_admin) do
   ActiveAdmin::ResourceController.prepend(Module.new do
     def restrict_format_access!
-      return if request.format.symbol == :all
+      return if request.format.symbol == :all || request.format.symbol == :html || request.format.nil?
       super
     end
   end)
