a10a425288bcf79dc8ec639e227a639eaba34246
Andrius Dereviancenko a.dereviancenko@gmail.com <a.dereviancenko@gmail.com>
2026-04-21 18:51:03 +0200

    Send contact emails synchronously

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
     message = params[:message].to_s.strip
 
     if name.present? && email.present? && message.present?
-      ContactMailer.contact_email(name: name, email: email, message: message).deliver_later
+      ContactMailer.contact_email(name: name, email: email, message: message).deliver_now
       render json: { ok: true }
     else
       render json: { ok: false, error: "All fields are required." }, status: :unprocessable_entity
